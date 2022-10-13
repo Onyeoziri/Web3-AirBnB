12f05c91f21c4855e4bd1e6a72410b45ca7285ea
Onyeoziri <75966640+Onyeoziri@users.noreply.github.com>
2022-10-12 22:53:22 -0400

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export {default as Home} from './home/Home';
+export {default as Login} from './login/Login';
+export {default as About} from './about/About';
+
+export {default as ErrorPage} from './errorPage/ErrorPage';
+
+
